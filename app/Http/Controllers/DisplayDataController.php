53857d13dbb4b72dc8afcf8da59f222b60ad22ea
kenichi ishii <ken1141@gmail.com>
2018-09-30 18:00:42 +0900

    コントローラーを追加

--- app/Http/Controllers/DisplayDataController.php
+++ app/Http/Controllers/DisplayDataController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\CarData;
 use Illuminate\Http\Request;
 
 class DisplayDataController extends Controller
 {
+    const DISPLAY_PER_PAGE = 50;
     //
     public function display()
     {
-        return view('display');
+        $list = CarData::paginate(self::DISPLAY_PER_PAGE);
+
+        return view('display', compact('list'));
     }
 }
