05582ec6ee42bbae9b24e418745c84470c4902f2
kenichi ishii <ken1141@gmail.com>
2018-09-30 17:19:54 +0900

    データ表示用のファイルを追加

---
+++ app/Http/Controllers/DisplayDataController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DisplayDataController extends Controller
+{
+    //
+    public function display()
+    {
+        return view('display');
+    }
+}
